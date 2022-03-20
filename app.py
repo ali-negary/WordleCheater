131a219e25bf14f6263ccfc7342711f505fce791
AliNegary <anegary@interrodata.com>
2022-03-20 09:53:24 +0330

    changed-input-format

--- app.py
+++ app.py
@@ -3,25 +3,27 @@ from main.suggest_words import SuggestWords
 if __name__ == '__main__':
     # length = input('How many letters in it?')
     length = 5
-    # print("Enter the letters:")
-    # word (space) state ==> states: (g)reen, (b)lack, (y)ellow
+    print("Enter your guess letter by letter:")
+    print("(letter (space) state ==> states: (g)reen, (b)lack, (y)ellow.")
+    # initial values.
     found = False
-    letterssss = [
-        [{'letter': 'D', 'state': 'y'}, {'letter': 'E', 'state': 'y'},
-         {'letter': 'A', 'state': 'y'}, {'letter': 'L', 'state': 'y'},
-         {'letter': 'T', 'state': 'b'}],
-        [{'letter': '', 'state': ''}, {'letter': '', 'state': ''},
-         {'letter': '', 'state': ''}, {'letter': '', 'state': ''},
-         {'letter': '', 'state': ''}]
-    ]
     i = 0
+
+    # initiate the suggester object.
+    suggest = SuggestWords(length)
+
+    # get input, find the word!
     while not found:
-        # letters = []
-        # for i in range(1, int(length) + 1):
-        #     letter, state = input(f'Letter{i} (space) State').split(' ')
+        word = input("Enter your guess: ")
+        status = input("Enter corresponding status of your guess: ")
+        letters = [{'letter': word[index].upper(), 'state': status[index]}
+                   for index in range(len(word))]
+        # for index in range(1, int(length) + 1):
+        #     letter, state = input(f'Letter{index} State\n').split(' ')
         #     letters.append({'letter': letter.upper(), 'state': state})
-        letters = letterssss[i]
-        guess = ''.join([i['letter'] for i in letters])
-        next_sug = SuggestWords(letters=letters, length=length).next_suggestion()
-        print(next_sug)
+        print(f"current guess: {word.upper()}")
+        suggest.next_suggestion(letters)
         i += 1
+        if i > 4:
+            print('You ran out of guesses!')
+            raise SystemExit
