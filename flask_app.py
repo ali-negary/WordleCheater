fc71620bf37d99666ffab40ad1ead5af4d5e8538
ali-negary <ali.negary@outlook.com>
2022-04-22 21:39:31 +0430

    deploy-to-heroku

--- flask_app.py
+++ flask_app.py
@@ -3,14 +3,9 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/home")
 def index():
-    return "Hello, World!"
-
-
-@app.route("/homepage")
-def homepage():
-    return "This is the homepage."
+    return "In case you did not notice, this is the Homepage."
 
 
 if __name__ == "__main__":
