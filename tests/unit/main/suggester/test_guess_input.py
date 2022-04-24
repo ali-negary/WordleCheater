db18d0772cc74533acf227503d106fffc43a512d
ali-negary <ali.negary@outlook.com>
2022-04-25 00:35:45 +0430

    added-unittest-for-guess_input

--- tests/unit/main/suggester/test_guess_input.py
+++ tests/unit/main/suggester/test_guess_input.py
@@ -1,34 +1,74 @@
 from unittest import TestCase
+from unittest.mock import Mock, patch
 
-from suggester.suggest_words import SuggestWords
+from suggester.guess_input import UserInput
 
 
 class TestSuggestWords(TestCase):
     def setUp(self):
-        self.suggest_words = SuggestWords(length=5)
+        self.user_input = UserInput()
 
-    def test_user_input_successful(
-        self,
-    ):
-        pass
+    def test_user_input_successful_ggbby(self):
+        expected_letters = ["D", "E", "A", "L", "T"]
+        expected_states = ["g", "g", "b", "b", "y"]
 
-    def test_validate_input_successful(
-        self,
-    ):
-        pass
+        # Mock the input function
+        side_effect = ["dealt", "ggbby"]
+        with patch("builtins.input", side_effect=side_effect):
+            self.user_input.user_input()
+            res = self.user_input.word
+            states = [item["state"] for item in res]
+            letters = [item["letter"] for item in res]
+            self.assertEqual(states, expected_states)
+            self.assertEqual(letters, expected_letters)
 
-    def test_validate_input_wrong_len(
-        self,
-    ):
-        pass
+    def test_user_input_successful_one_g(self):
+        expected_letters = ["D", "E", "A", "L", "T"]
+        expected_states = ["g", "g", "g", "g", "g"]
 
-    def test_validate_input_wrong_states(
-        self,
-    ):
-        pass
+        # Mock the input function
+        side_effect = ["dealt", "g"]
+        with patch("builtins.input", side_effect=side_effect):
+            self.user_input.user_input()
+            res = self.user_input.word
+            states = [item["state"] for item in res]
+            letters = [item["letter"] for item in res]
+            self.assertEqual(states, expected_states)
+            self.assertEqual(letters, expected_letters)
 
-    def test_grab_word(self):
-        pass
+    def test_validate_input_true(self):
+        self.user_input.word = [
+            {"letter": "D", "state": "g"},
+            {"letter": "E", "state": "g"},
+            {"letter": "A", "state": "b"},
+            {"letter": "L", "state": "b"},
+            {"letter": "T", "state": "y"},
+        ]
+        self.assertTrue(self.user_input.validate_input())
+
+    def test_validate_input_false_length(self):
+        self.user_input.word = [
+            {"letter": "D", "state": "g"},
+            {"letter": "E", "state": "g"},
+            {"letter": "A", "state": "b"},
+            {"letter": "L", "state": "b"},
+        ]
+        self.assertFalse(self.user_input.validate_input())
+
+    def test_validate_input_false_state(self):
+        self.user_input.word = [
+            {"letter": "D", "state": "z"},
+            {"letter": "E", "state": "g"},
+            {"letter": "A", "state": "b"},
+            {"letter": "L", "state": "b"},
+            {"letter": "T", "state": "y"},
+        ]
+        self.assertFalse(self.user_input.validate_input())
+
+    def test_get_word_invalid(self):
+        self.user_input.user_input = Mock()
+        self.user_input.validate_input = Mock(return_value=False)
+        self.assertFalse(self.user_input.get_word())
 
     def tearDown(self) -> None:
-        pass
+        self.user_input = None
